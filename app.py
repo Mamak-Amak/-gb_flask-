aad237e70de75f394fb2c8f712780ba88dd27f7a
Mamak-Amak <105986370+Mamak-Amak@users.noreply.github.com>
2023-05-02 18:40:56 +0500

    Add files via upload (#2)

---
+++ app.py
@@ -0,0 +1,28 @@
+from blog.models.database import db
+from flask import Flask, render_template
+from blog.views.auth import login_manager, auth_app
+
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+from blog.views.users import users_app
+
+app.register_blueprint(users_app, url_prefix="/users")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/blog.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
+app.config["SECRET_KEY"] = "abcdefg123456"
+app.register_blueprint(auth_app, url_prefix="/auth")
+login_manager.init_app(app)
